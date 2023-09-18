3b8bb95a883886e15a0e924f29e9c1f6ca3d0777
llpingll <94099281+llpingll@users.noreply.github.com>
2023-09-18 21:53:39 +0100

    Add error checks | better separation of concerns & encapsulation

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-const input = document.querySelector("input");
-const form = document.querySelector("form");
+'use strict';
 
-const getCurrentTemp = (location) => {
-    const baseURL = "http://api.weatherapi.com/v1/current.json?";
-    const key = "key=57b222cdee564ac38e3110636232508";
+// Form module
+const form = (() => {
+    const formDOM = document.querySelector("form");
+    const input = document.querySelector("input");
 
-    fetch(`${baseURL}${key}&q=${location}`, {mode: "cors"})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response);
-    });
-}
+    const getCurrentTemp = () => {
+        const baseURL = "http://api.weatherapi.com/v1/current.json?";
+        const key = "key=57b222cdee564ac38e3110636232508";
 
-form.addEventListener("submit", (e) => {
+        return fetch(`${baseURL}${key}&q=${input.value}`, {mode: "cors"})
+        .then(function (response) {
+            if (!response.ok) {
+                if (response.status === 400) {
+                    // console.log("Error 400");
+                    throw new Error(`Error: City ${input.value} not found`);
+                } else {
+                    // console.log("Error failed");
+                    throw new Error("Failed to retrieve data");
+                }
+            }
+            return response.json();
+        });
+    }
+
+    return {input, formDOM, getCurrentTemp};
+})();
+
+form.formDOM.addEventListener("submit", (e) => {
     e.preventDefault();
-    getCurrentTemp(input.value);
+
+    if (!form.input.value.trim()) {
+        return;
+    }
+
+    form.getCurrentTemp()
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        alert(error.message);
+    });
+    // .finally(display.render());
+    formDOM.reset();
 });
